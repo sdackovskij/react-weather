c9895c806d9e2074539efd8542cfb97cd7493efc
sdackovskij <41028149+sdackovskij@users.noreply.github.com>
2019-09-13 22:05:43 +0300

    V1

---
+++ src/components/weather.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class Weather extends React.Component{
+    render () {
+        return (
+        <div className = "infoWeath">
+            {
+            this.props.city &&
+        
+            <div>
+            <p>Местоположение: {this.props.city}, {this.props.country}</p>
+            <p>Температура: {this.props.temp}</p>
+            <p>Давление: {this.props.pressure}</p>
+            <p>Заход Солнца: {this.props.sunset}</p>
+            </div>
+        }
+            <p className = "error">{this.props.error}</p>
+        </div>
+        );
+    }
+}
+
+export default Weather;
